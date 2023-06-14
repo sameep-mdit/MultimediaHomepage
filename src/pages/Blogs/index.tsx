f7280ad2eb2524584e1ae96f67eedf699c14419b
m-it-Bibhu <kiju.bibhu.mdit@gmail.com>
2023-06-14 11:18:29 +0545

    error solved

---
+++ src/pages/Blogs/index.tsx
@@ -0,0 +1,10 @@
+import MainWrapper from "../../Layout/MainWrapper";
+const Blogpage = () => {
+  return (
+    <MainWrapper>
+      <div>hello from Blog</div>
+    </MainWrapper>
+  );
+};
+
+export default Blogpage;
--- src/pages/Home/LandingPage.tsx
+++ src/pages/Home/LandingPage.tsx
@@ -12,10 +12,10 @@ import { SvgBlob } from "react-svg-blob";
 const LandingPage = () => {
   const homePageData = React.useContext(HomeContext);
   React.useEffect(() => {
-    console.log(homePageData);
+    console.log(homePageData, "after fetched");
   }, [homePageData]);
   return (
-    <div className="h-[90vh] flex-wrap-reverse bg-[#003455] text-white flex flex-col-reverse md:px-[20vh] md:flex-row px-10">
+    <div className="h-[90vh] flex-wrap-reverse  bg-[#003455] text-white flex flex-col-reverse md:px-[20vh] md:flex-row ">
       <div className="flex-1 flex  flex-col-reverse md:flex md:flex-row items-center">
         <div className="flex  md:flex-col my-4  md:justify-evenly">
           <FontAwesomeIcon
@@ -46,7 +46,7 @@ const LandingPage = () => {
           variant="image"
           className="hover:cursor-pointer  h-full puff-in-center "
           image="https://scontent.fktm10-1.fna.fbcdn.net/v/t1.6435-9/126212254_1143698759361370_982448368751155537_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=174925&_nc_ohc=0w4oPqwkaiYAX_Mck2A&_nc_ht=scontent.fktm10-1.fna&oh=00_AfAKr9oRWS2RwWvRP3PcPk9ri20H5gUgFrurnz0I3jYleg&oe=64AFFF90"
-          shapeProps={{ edges: 3, growth: 8, size: 340 }}
+          shapeProps={{ edges: 3, growth: 9, size: 460 }}
         />
       </div>
     </div>
