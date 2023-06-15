ad466c7f2ca60e3a93f883a1ce7386ac0dcce54a
m-it-Bibhu <kiju.bibhu.mdit@gmail.com>
2023-06-15 12:12:26 +0545

    WIP video page

--- src/component/global/Cards/VideoCard/index.tsx
+++ src/component/global/Cards/VideoCard/index.tsx
@@ -5,16 +5,32 @@ type IVideocard = {
   title: string;
   desc: string;
   link: string;
+  createdDate: string;
+  index: number;
 };
 
-const VideoCard: React.FC<IVideocard> = ({ title, desc, link }) => {
-  console.log(link, desc);
+const VideoCard: React.FC<IVideocard> = ({
+  title,
+  link,
+  createdDate,
+  index,
+}) => {
   return (
-    <div className="mr-5 rounded">
-      <div className="w-full h-54 pointer-events-none ">
+    <div
+      className={
+        index % 2 == 0
+          ? "mr-5 rounded w-[350px] h-[360px] text-white"
+          : "mr-5 rounded w-[350px] h-[360px] text-black"
+      }
+    >
+      <div className=" h-4/5 pointer-events-none">
         <ReactPlayer width="100%" height="100%" url={link} />
       </div>
-      <p>{title}</p>
+      <div className="flex flex-col p-2 gap-1">
+        <h4>{title}</h4>
+        <hr />
+        <p>{createdDate.split("T")[0]}</p>
+      </div>
     </div>
   );
 };
--- src/pages/Home/LandingPage.tsx
+++ src/pages/Home/LandingPage.tsx
@@ -11,14 +11,15 @@ import { SvgBlob } from "react-svg-blob";
 import { baseUrl } from "../../constants/Strings";
 
 const LandingPage = () => {
+  console.log(baseUrl);
   const homePageData = React.useContext(HomeContext);
   React.useEffect(() => {
     console.log(homePageData, "after fetched");
   }, [homePageData]);
   return (
-    <div className="h-[90vh] px-8 w-full grid md:grid-cols-2  bg-[#003455] text-white">
-      <div className="flex-1 flex  flex-col-reverse md:flex md:flex-row items-center">
-        <div className="flex  md:flex-col my-4  md:justify-evenly">
+    <div className="h-[90vh] md:px-[20vh] w-full flex flex-col-reverse bg-white md:flex-row">
+      <div className="flex-1 flex flex-col-reverse md:flex md:flex-row items-center">
+        <div className="flex md:flex-col my-4  md:justify-evenly">
           <FontAwesomeIcon
             icon={faFacebookSquare}
             size="2x"
@@ -47,8 +48,9 @@ const LandingPage = () => {
         <SvgBlob
           variant="image"
           className="hover:cursor-pointer w-full  h-full puff-in-center object-contain"
-          image={`${baseUrl}/uploads/${homePageData.data?.homepage.photo}`}
-          shapeProps={{ edges: 3, growth: 9,size:400}}
+          // image={`${baseUrl}/uploads/${homePageData.data?.homepage.photo}`}
+          image="https://scontent.fktm20-1.fna.fbcdn.net/v/t39.30808-6/315751126_1630743573990217_4323941406538844063_n.jpg?_nc_cat=105&cb=99be929b-59f725be&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=uENCKo6uMmwAX8ffMSm&_nc_ht=scontent.fktm20-1.fna&oh=00_AfBTWaDHjrB5LP-1gkdc2X-q3MN_aTind1WCU4gtC3angw&oe=648FB9B4"
+          shapeProps={{ edges: 3, growth: 9, size: 400 }}
         />
       </div>
     </div>
